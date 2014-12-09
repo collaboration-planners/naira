813ceb8306df6d620fb1dae26b38395469fdb428
jf@collaboration-planners.com <jean.f.cloutier@gmail.com>
2014-12-08 22:09:05 -0500

    Alchemist config and renamed TopSupervisor to MainSupervisor

---
+++ lib/naira/main_supervisor.ex
@@ -0,0 +1,28 @@
+defmodule Naira.MainSupervisor do
+	@moduledoc """
+Naira's main supervisor. It supervises the Event Manager, the Atom Pool and the Stream Supervisor.
+It is itself supervised by the Phoenix supervisor.
+"""
+	@name __MODULE__
+	use Supervisor
+
+	### Supervisor Callbacks
+
+	@spec start_link() :: {:ok, pid}
+  def start_link() do
+		IO.puts "Starting main supervisor"
+		{:ok, _pid} = Supervisor.start_link(@name, [], [name: @name])
+	end 
+
+	@spec init(any) :: {:ok, tuple}
+	def init(_) do
+		children = [
+								 worker(Naira.EventManager, []),
+								 worker(Naira.AtomPool, [100]), #pre-populate atom pool with 100 reusable atoms
+								 supervisor(Naira.StreamsSupervisor, [])
+						   ]
+		opts = [strategy: :one_for_one]
+		supervise(children, opts)
+	end
+
+end
